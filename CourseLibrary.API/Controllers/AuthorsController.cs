24fe32a9a849b2a40d3a6eeea53c1466aa9eca5e
farrukhkhan89 <farrukh_mask@Hotmail.com>
2019-11-29 17:13:15 +0500

    Code for dotnetcore3 Api

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Helpers;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository,
+            IMapper mapper)
+        {
+
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ?? throw new ArgumentOutOfRangeException(nameof(mapper));
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authorList = _courseLibraryRepository.GetAuthors();
+            return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authorList));
+
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var author = _courseLibraryRepository.GetAuthor(authorId);
+            if (author != null)
+                return Ok(_mapper.Map<AuthorDto>(author));
+
+            return NotFound();
+
+        }
+    }
+}
